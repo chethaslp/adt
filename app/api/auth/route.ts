5cbf56febe741576c73ecce325bcd89b68817937
chethas.lp <lchethas@gmail.com>
2024-05-09 03:04:29 +0530

    minor fix

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -7,25 +7,8 @@ import keys from "@/keys.json"
 
 
 export async function POST(req: Request) {
-
-    const { code, uid }  = await req.json()
-  
-    if (!code || !uid) {
       return NextResponse.json(
         { msg: 'Missing required arguments.' },
         { status: 400 }
       );
-    }
-    const oAuth2Client = new OAuth2Client(
-        keys.web.client_id,
-        keys.web.client_secret,
-        "http://localhost:3000/api/auth"
-
-      );
-    const r = await oAuth2Client.getToken(code);
-  
-    return NextResponse.json({
-      at: r.tokens.access_token,
-      rt: r.tokens.refresh_token,
-      exp: r.tokens.expiry_date});
   }
