0de1f273175ec5d6c2724e60d114c5f780431197
chethas.lp <lchethas@gmail.com>
2024-05-09 14:59:08 +0530

    minor fix

--- app/sheet/[sid]/page.tsx
+++ app/sheet/[sid]/page.tsx
@@ -3,34 +3,13 @@
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Navbar } from '@/components/ui/navbar';
-import { FcGoogle } from "react-icons/fc";
-import { SiGooglesheets } from "react-icons/si";
 import { useToast } from '@/components/ui/use-toast';
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { useAuthContext } from "@/components/context/auth";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { GoogleAuthProvider, User, signInWithPopup } from 'firebase/auth';
-import { auth } from '@/components/fb/config';
-import { ClsType, UserType, createUser, getUser, getUserClses } from '@/components/fb/db';
-import { NewSheetDialog } from '@/components/dialog/create-sheet';
+import { User, useAuthContext } from "@/components/context/auth";
 import { useEffect, useState } from 'react';
-import { HashLoader } from 'react-spinners'
-import { redirect, useRouter, useSearchParams } from 'next/navigation';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { useGoogleLogin } from '@react-oauth/google';
 import Loading from '@/app/loading';
 import { DocumentData, QueryDocumentSnapshot, Timestamp } from 'firebase/firestore';
-import timeAgo from '@/lib/showtimeago'
 import { Separator } from '@/components/ui/separator';
-import { SigninDialog } from '@/components/dialog/signin-dialog';
+import { useRouter } from 'next/navigation';
 
 
 export default function Home({ params }: {params: {sid:string}}) {
@@ -44,18 +23,11 @@ export default function Home({ params }: {params: {sid:string}}) {
   const [isNewSheetDialogOpen, setNewSheetDialogOpen] = useState(false)
   const [isSigninDialogOpen, setSigninDialogOpen] = useState(false)
 
-  const s = useSearchParams()
-
   useEffect(()=>{
     if(!user) {
       router.push('/')
-      return
     }
-    
-    getUserClses(user).then((data)=>{
-      setUserClses(data)
-      setLoading(false)
-    })
+    setLoading(false)
    
   },[user,router])
 
